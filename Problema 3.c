e2b1584319a61e1eb98c85219231f33f78f92e7d
Estuardo <3354094580901@ingenieria.usac.edu.gt>
2023-08-20 12:11:01 -0600

    Problema 3 finalizado

---
+++ Problema 3.c
@@ -0,0 +1,29 @@
+/**
+- El usuario ingresa una cadena de texto
+- El programa cuenta la cantidad de caracteres en la cadena
+- El programa muestra la cantidad de caracteres 
+*/
+
+#import "Header.h"
+
+int main ()
+{
+    char text[140];
+    int counter = 0;
+
+    clean(1);
+    
+    // Interfaz para el ingreso de la cadena
+    printf("CONTADOR DE CARACTERES\n");
+    printf("Escriba a continuación: \n");
+    printf("\n------------------------------------------------------------\n");
+    scanf("%139[^\n]", text);
+
+    // Operatoria
+    for (int i = 0; text[i] != '\0'; i++) if (text[i] != ' ') counter ++; 
+    printf("\nNúmero de caracteres: %i\n", counter);
+    
+    clean(3);
+
+    return 0;
+}
